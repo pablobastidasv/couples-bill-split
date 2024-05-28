721861458ad8151c1f195cd3632e57e33aca13b3
Pablo Bastidas <pablobastidasv@gmail.com>
2024-05-28 08:03:21 +0200

    changes on actions for IT and UT

--- lib/core/views/app_router.dart
+++ lib/core/views/app_router.dart
@@ -1,11 +1,9 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:couple_budget_calculator/budget/calculator/views/pages/budget_split.dart';
 import 'package:couple_budget_calculator/budget/calculator/views/pages/settings.dart';
-import 'package:flutter/material.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'app_router.g.dart';
-
 part 'app_router.gr.dart';
 
 @AutoRouterConfig(
